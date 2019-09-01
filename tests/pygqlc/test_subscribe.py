9bd30056ac60d80c28ff220a73def5cfecbae01e
Baruc Almaguer <baruc.almaguer@gmail.com>
2019-09-01 19:25:55 -0400

    refactor subscription demo

--- tests/pygqlc/test_subscribe.py
+++ tests/pygqlc/test_subscribe.py
@@ -1,7 +1,8 @@
 from pygqlc import GraphQLClient
 from tests.pygqlc import subscriptions as subs
 from tests.pygqlc import mutations as muts
-gql = GraphQLClient()
+# gql = GraphQLClient()
+
 def on_author_updated(msg):
   if(msg['successful']):
     author = msg['result']
@@ -16,27 +17,15 @@ def on_author_created(msg):
   else:
     print(f'error creating author: {msg["messages"]}')
 
-unsub_1 = gql.subscribe(subs.sub_author_updated, callback=on_author_updated)
-unsub_2 = gql.subscribe(subs.sub_author_created, callback=on_author_created)
-data, errors = gql.mutate(muts.update_any_author_active, {'name': 'Baruc', 'active': True})
-data, errors = gql.mutate(muts.create_author, {'name': 'Juanito', 'lastName': 'Saldi'})
-wss_msg = {
-  'type': 'data',
-  'id': 0,
-  'payload': {
-    'data': {
-      'authorUpdated': {
-        'successful': True, 
-        'messages': [], 
-        'result': {
-          'id': '7', 
-          'name': 'Baruc', 
-          'lastName': 'Almaguer', 
-          'active': True, 
-          'dateOfBirth': None, 
-          'updatedAt': '2019-09-01T18:05:55Z'
-        }
-      }
-    }
-  }
-}
+
+# unsub_1 = gql.subscribe(subs.sub_author_updated, callback=on_author_updated)
+# unsub_2 = gql.subscribe(subs.sub_author_created, callback=on_author_created)
+
+# ! To trigger subscription:
+# data, errors = gql.mutate(muts.update_any_author_active, {'name': 'Baruc', 'active': True})
+# data, errors = gql.mutate(muts.update_any_author_active, {'name': 'Baruc', 'active': False})
+# data, errors = gql.mutate(muts.create_author, {'name': 'Juanito', 'lastName': 'Saldi'})
+
+# ! to exit:
+# >> gql.close()
+# >> exit()
