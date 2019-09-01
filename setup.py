cd97939a967c620c5a9a85e66695628430014eb7
Baruc Almaguer <baruc.almaguer@gmail.com>
2019-08-31 22:22:11 -0400

    Add pypi dependencies

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 desc = "GraphQL API Client for python language"
 setup(
-    name='pygqlc',
+    name='pygqlc-valiot',
     version=version,
     description=desc,
     author="Baruc Almaguer",
