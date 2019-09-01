cdd3d5086bcaea8f187f02e9091b74210ed175e2
Baruc Almaguer <baruc.almaguer@gmail.com>
2019-09-01 02:01:08 -0400

    Add coverage dependency, finish QUERY function coverage

--- tests/pygqlc/test_query.py
+++ tests/pygqlc/test_query.py
@@ -1,8 +1,21 @@
+import pydash as _py
 import queries
 def test_query_no_errors(gql):
   _, errors = gql.query(queries.get_authors)
-  assert errors == []
+  assert errors == [], \
+    'query must NOT contain errors'
 
 def test_query_has_errors(gql):
   _, errors = gql.query(queries.bad_get_authors)
-  assert len(errors) > 0
+  assert len(errors) > 0, \
+    'query MUST contain errors'
+
+def test_query_flatten(gql):
+  data, _ = gql.query(queries.get_authors, flatten=True)
+  assert not _py.get(data, 'data'), \
+    'data must NOT appear as response root'
+
+def test_query_not_flatten(gql):
+  data, _ = gql.query(queries.get_authors, flatten=False)
+  assert _py.get(data, 'data'),  \
+    'data must appear as response root'
