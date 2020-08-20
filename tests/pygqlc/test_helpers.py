e6d6b925c57a9a557abfdc962ed45586c4174042
Alan Rocha <alan.rocha@udem.edu>
2020-08-20 11:19:56 -0500

    Created Singleton call test

--- tests/pygqlc/test_helpers.py
+++ tests/pygqlc/test_helpers.py
@@ -1,4 +1,5 @@
 from pygqlc.GraphQLClient import safe_pop
+from pygqlc.helper_modules.Singleton import Singleton
 
 def test_safe_pop_first():
   data = [3, 4, 5]
@@ -20,11 +21,26 @@ def test_safe_pop_empty():
 def test_safe_pop_empty_default():
   datum = safe_pop([], default=0)
   assert datum == 0, \
-      'return value should be 0 by default'
+    'return value should be 0 by default'
 
 
 def test_safe_pop_index_default():
   data = [3, 4, 5]
   datum = safe_pop(data)
   assert datum == 3, \
-      'return value should be the first element by default'
+    'return value should be the first element by default'
+
+def test_singleton_cannot_be_instantiated_twice():
+  class UselessLetterClass(metaclass=Singleton):
+
+    def __init__(self, letter='G'):
+      self.letter = letter
+
+  first_instance = UselessLetterClass('A')   # First call: actually creates a new instance
+  second_instance = UselessLetterClass() # Second call: returns the cached instance
+  assert first_instance is second_instance, \
+    'Should be the same instance'
+  del Singleton._instances[UselessLetterClass]  # Throw away the cached instance
+  third_instance = UselessLetterClass()   # Third call: no cached instance, so create one
+  assert first_instance is not third_instance, \
+    'Should be a different instance'
