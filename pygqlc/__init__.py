979fe0cb9b8fb84d27a0b61918436b3e082185c7
Alan Rocha <alan.rocha@udem.edu>
2020-09-02 12:16:20 -0500

    Added init file

--- pygqlc/__init__.py
+++ pygqlc/__init__.py
@@ -2,7 +2,6 @@ from .GraphQLClient import GraphQLClient
 from .QueryParser import QueryParser
 from .MutationParser import MutationParser
 from .SubscriptionParser import SubscriptionParser
-from .helper_modules.Singleton import Singleton
 
 # * Package name:
 name = 'pygqlc'
